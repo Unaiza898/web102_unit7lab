52d37906a214983560acbb496a16fe4304ee9045
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2024-04-13 15:09:37 -0400

    commit

--- client/src/client.js
+++ client/src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-const URL = process.env.REACT_APP_URL;
-const API_KEY = process.env.REACT_APP_API_KEY;
+const URL = "your key";
+const API_KEY = "your key";
 export const supabase = createClient(URL, API_KEY);
 
